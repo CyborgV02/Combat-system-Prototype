87cd5c1e2a2fbde0b27072b89238d5d19d71fff6
Cyborg <ahmedkamalv2@gmail.com>
2026-01-20 06:19:31 +0200

    Implemented Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,22 @@ public class PlayerController : MonoBehaviour
    [SerializeField] private float jumpForce = 7.0f;
    private Rigidbody2D rb;
    public Animator anim;
+   public bool isGrounded;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
+
+    void Update()
+    {
+       jump();
+    }
     void FixedUpdate()
     {
+
+       isGrounded = Physics2D.CircleCast(transform.position, 0.1f, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+        anim.SetBool("Grounded", isGrounded);
         float moveX = Input.GetAxisRaw("Horizontal");
         rb.linearVelocityX = moveX * movementSpeed;
 
@@ -24,4 +33,14 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("Speed", Mathf.Abs(moveX));
     }
+
+    private void jump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            anim.SetBool("Grounded", false);
+        }
+    }
+
 }
