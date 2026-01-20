3b7ccbfede058783cc38fb36420fbd534e3ef0bf
Cyborg <ahmedkamalv2@gmail.com>
2026-01-20 05:39:04 +0200

    Implemented Movement with animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+   [SerializeField] private float movementSpeed = 5.0f;
+   [SerializeField] private float jumpForce = 7.0f;
+   private Rigidbody2D rb;
+   public Animator anim;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocityX = moveX * movementSpeed;
+
+         if (moveX > 0)
+        transform.localScale = new Vector2(-1, 1);
+    else if (moveX < 0)
+        transform.localScale = new Vector2(1, 1);
+
+        anim.SetFloat("Speed", Mathf.Abs(moveX));
     }
 }
